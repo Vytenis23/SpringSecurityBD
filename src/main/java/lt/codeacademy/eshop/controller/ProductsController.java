f870fdbc2584fec1472757ecd9a4df8df83d7143
Rokas Ulickas <rokas@recodin.lt>
2023-11-16 21:08:16 +0200

    implement products get all method

--- src/main/java/lt/codeacademy/eshop/controller/ProductsController.java
+++ src/main/java/lt/codeacademy/eshop/controller/ProductsController.java
@@ -9,79 +9,23 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.UUID;
+import java.util.List;import java.util.UUID;
 
-@Controller
+@RestController
 @RequestMapping("/products")
+@CrossOrigin("*")
 public class ProductsController {
 
     private final ProductService productService;
-    private final MessageService messageService;
 
-    public ProductsController(@Qualifier("productServiceImpl") ProductService productService, MessageService messageService) {
+    public ProductsController(@Qualifier("productServiceImpl") ProductService productService) {
         this.productService = productService;
-        this.messageService = messageService;
-    }
-
-    @GetMapping("/create")
-    public String openCreateProductForm(Model model, String message) {
-        model.addAttribute("product", new Product());
-        if (message != null) {
-            model.addAttribute("success", messageService.getMessage(message));
-        }
-
-        return "product";
-    }
-
-    @PostMapping("/create")
-    public String createProduct(Product product) {
-        productService.addProduct(product);
-
-        return "redirect:/products/create?message=create.product.success.message";
     }
 
     @GetMapping
-    public String getProducts(
-            @PageableDefault(size = 5, sort = {"name"}, direction = Sort.Direction.ASC) Pageable pageable,
-            Model model) {
-        model.addAttribute("productsPage", productService.getProductsPaginated(pageable));
-
-        return "products";
+    public List<Product> getAll() {
+        return productService.getProducts();
     }
-
-    @GetMapping("/findByName")
-    public String getProductByName(@RequestParam String name, Model model) {
-
-        model.addAttribute("product", productService.getByName(name));
-
-        return "product";
-    }
-
-    @GetMapping("/update")
-    public String updateProduct(@RequestParam UUID id, Model model) {
-        Product product = productService.getProduct(id);
-
-        model.addAttribute("product", product);
-
-        return "product";
-    }
-
-    @PostMapping("/update")
-    public String updateProject(Product product, Model model) {
-        productService.update(product);
-
-        return "redirect:/products";
-    }
-
-    @GetMapping("/delete")
-    public String deleteProduct(@RequestParam UUID id, Model model) {
-        productService.delete(id);
-        return "redirect:/products";
-    }
-
 }
